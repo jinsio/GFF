5600babc47fe8b01d7e8700ccb432d302c323c88
fca406-23 <>
2022-12-13 12:22:12 +0900

    検知処理追加

--- G_game/Monster/Slime.h
+++ G_game/Monster/Slime.h
@@ -18,5 +18,23 @@ public:
     /// </summary>
     ~Slime();
 
+    /// <summary>
+    /// スライムの更新処理
+    /// </summary>
+    /// <param name="deltaTime">1フレームの経過時間</param>
+    void Update(float deltaTime) override;
+
 private:
+    /// <summary>
+    /// 見つけているかの確認
+    /// </summary>
+    /// <returns>見つけていたらtrue そうでなければfalse</returns>
+    bool IsFind(float playerPosX);
+
+    // 自動移動秒数
+    //float autoMoveCount;
+
+    // 検知できる範囲
+    const float findRange = 160.0f;
+
 };
